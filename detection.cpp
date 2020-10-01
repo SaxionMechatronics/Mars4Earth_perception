b73b2deed6e1a47f4c4e712e711fffa9654e70d1
Wytse de Witte <wytsedewitte@gmail.com>
2020-10-01 13:42:07 +0200

    Code cleanup Added blur methods for the canny transform

--- detection.cpp
+++ detection.cpp
@@ -5,19 +5,15 @@
   @email    thijsdejong21@gmail.com
   @date     16th of May, 2019
 */
-
-
 //class header
 #include "detection.hpp"
-
 //Namespace
 using namespace std; 
 using namespace cv;
 using namespace cv::xfeatures2d; 
 
 Detector::Detector(){
-        //created named windows, for the trackbars
-        window_capture_name = "Video Capture";
+
     };
 
 void Detector::locate(Mat frame) {
@@ -62,28 +58,19 @@ void Detector::locate(Mat frame) {
     drawKeypoints( blob, keypoints, im_with_keypoints, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
 
     // Show blobs
-    ///imshow("keypoints", im_with_keypoints );
+    imshow("keypoints", im_with_keypoints );
 
     //Debug print
     for( int i = 0 ; i < keypoints.size(); i++)
     {
         cout << "Blob " << i << " : " << keypoints[i].pt << "\t";
-
     }
-    
     cout << "\n\n";
     //PnP magic ...
     std::vector<cv::Point2d> image_points;
 
     if(keypoints.size() == 5)
     {
-        
-        // image_points.push_back( cv::Point2d(257, 394) );    
-        // image_points.push_back( cv::Point2d(371, 295) );    
-        // image_points.push_back( cv::Point2d(268, 288) );     
-        // image_points.push_back( cv::Point2d(165, 279) ); 
-        // image_points.push_back( cv::Point2d(278, 186) ); 
-
         //Fill image_points using blob detection
 
         image_points.push_back( cv::Point2d(keypoints[0].pt.x, keypoints[0].pt.y) );    
@@ -108,8 +95,6 @@ void Detector::locate(Mat frame) {
         cv::Mat dist_coeffs = cv::Mat::zeros(4,1,cv::DataType<double>::type); // Assuming no lens distortion
         
         cout << "Camera Matrix " << endl << camera_matrix << endl ;
-        
-        
         // Solve for pose
         cv::solvePnP(model_points, image_points, camera_matrix, dist_coeffs, rotation_vector, translation_vector);
         
@@ -130,26 +115,19 @@ void Detector::locate(Mat frame) {
         cv::line(frame,image_points[0], nose_end_point2D[0], cv::Scalar(255,0,0), 2);
         cv::line(frame,image_points[0], nose_end_point2D[1], cv::Scalar(0,255,0), 2);
         cv::line(frame,image_points[0], nose_end_point2D[2], cv::Scalar(0,0,255), 2);
-        
-        // int str = (rotation_vector.at<float>(0));
-        // stringstream iss;
-        // iss << str;
-        // putText(frame, iss.str(), Point2d(10,20), 1, 2, Scalar(255,0,0), 2);
+
         cout << "Rotation Vector " << endl << rotation_vector << endl;
         cout << "Translation Vector" << endl << translation_vector << endl;
         
         cout <<  nose_end_point2D << endl;
     }
-    // Display image.
-    cv::imshow("Output", frame);
-
 }
 
 vector<Point2d> Detector::detect(Mat frame){
     // -------- gray scaling ----------
-    Mat BW1;
-    cvtColor(frame, BW1, COLOR_BGR2GRAY);
-    // -------- Skeleton ---------- //
+    Mat BW;
+    cvtColor(frame, BW, COLOR_BGR2GRAY);
+//    // -------- Skeleton ---------- //
 //    cv::Mat skel(BW1.size(), CV_8UC1, cv::Scalar(0));
 //    cv::Mat temp;
 //    cv::Mat eroded;
@@ -174,82 +152,74 @@ vector<Point2d> Detector::detect(Mat frame){
 //    dilate(skel,BW1_5,element);
 //    imshow("bw1.5: skeleton", BW1_5);
 
+    //-------Canny----------//
+    Mat cannyT, gBlur, mBlur;
+    GaussianBlur(BW,gBlur, Size(5, 5), 3 );
+    medianBlur ( gBlur, mBlur, 3 );
+    Canny(mBlur,cannyT,75, 200, 3, true);
+    imshow("bw1.5: Canny", cannyT);
     //-------Hough lines----------//
-    Mat HLT1 = frame;
-    Canny(BW1,frame,50, 300, 3);
-    imshow("bw1.5: Canny", frame);
-    vector<Vec4i> lines;
-    HoughLinesP( frame, lines, 1, CV_PI/360, 50, 50, 10 );
     /*
-    vector<Vec4i> lines;
-    HoughLinesP(dst, lines, 1, CV_PI/180, 50, 50, 10 );
-    with the arguments:
     dst: Output of the edge detector. It should be a grayscale image (although in fact it is a binary one)
     lines: A vector that will store the parameters (x_{start}, y_{start}, x_{end}, y_{end}) of the detected lines
     rho : The resolution of the parameter r in pixels. We use 1 pixel.
     theta: The resolution of the parameter \theta in radians. We use 1 degree (CV_PI/180)
     threshold: The minimum number of intersections to “detect” a line
     minLinLength: The minimum number of points that can form a line. Lines with less than this number of points are disregarded.
-    maxLineGap: The maximum gap between two points to be considered in the same line.
-    */
+    maxLineGap: The maximum gap between two points to be considered in the same line.  */
+    vector<Vec4i> lines;
+    HoughLinesP( cannyT, lines, 1, CV_PI/360, 50, 50, 10 );
     for(auto & i : lines)
     {
-        line(HLT1, Point(i[0], i[1]), Point(i[2], i[3]), Scalar(0, 0, 255), 2, LINE_AA );
+        line(frame, Point(i[0], i[1]), Point(i[2], i[3]), Scalar(0, 0, 255), 2, LINE_AA );
     }
     Rect boundingBox = lines2boundingbox(frame, lines);
 
     //cout << "HLT1 \t box: " << 1 << "\t Xmin, Xmax: (" << Xmin << "," << Xmax << ") \t Ymin,Ymax: (" << Ymin << "," << Ymax  << ")\n";
-    rectangle(HLT1, boundingBox, Scalar(255, 0, 0));
+    rectangle(frame, boundingBox, Scalar(0, 0, 0,255));
     //imshow("bw4: Hough lines", HLT1);
     //Mat& img, Rect rec, const Scalar& color,
 
     //Group overlaying lines together
     vector<Vec4i> corelines = lines2points(lines);
-    
 
-
-    //----- Fabriacte points for the locate alg. -----------//
+    //----- Fabricate points for the locate alg. -----------//
     Size s = frame.size();
     // rows = s.height;
     // cols = s.width;
     vector<Point2d> output;
     vector<Point2d> centerpoints;
-
-    Mat BW5 = frame;
+    Mat circleEnd = frame;
     //circle(Mat& img, Point center, int radius, const Scalar& color, int thickness=1, int lineType=8, int shift=0)
 
-    //Mat BW5;
-
     //Determinens which end points of the found core lines are in the middle of the screen and which one are on the edges
-    for(size_t i = 0; i < corelines.size(); i++ )
+    for(auto & coreline : corelines)
     {
-        if( ( corelines[i][0] < (s.width/2 - s.width/4) || corelines[i][0] > (s.width/2 + s.width/4) )
-            ||
-            ( corelines[i][1] < (s.height/2 - s.height/4) || corelines[i][1] > (s.height/2 + s.height/4) )  )
+        if( ( coreline[0] < (s.width/2 - s.width/4) || coreline[0] > (s.width/2 + s.width/4) ) ||
+            ( coreline[1] < (s.height/2 - s.height/4) || coreline[1] > (s.height/2 + s.height/4) )  )
         {
-            //circle(BW5,Point2d(corelines[i][0], corelines[i][1]), 5, Scalar(0,0,255),2); //red
-            output.emplace_back(corelines[i][0], corelines[i][1]);
+            //circle(circle,Point2d(corelines[i][0], corelines[i][1]), 5, Scalar(0,0,255),2); //red
+            output.emplace_back(coreline[0], coreline[1]);
         }
         else
         {
-            //circle(BW5,Point2d(corelines[i][0], corelines[i][1]), 5, Scalar(255,255,0),2); //blue
-            centerpoints.emplace_back(corelines[i][0], corelines[i][1]);
+            //circle(circle,Point2d(corelines[i][0], corelines[i][1]), 5, Scalar(255,255,0),2); //blue
+            centerpoints.emplace_back(coreline[0], coreline[1]);
         }
 
-        if( ( corelines[i][2] < (s.width/2 - s.width/4) || corelines[i][2] > (s.width/2 + s.width/4) )
-            || 
-            (corelines[i][3] < (s.height/2 - s.height/4) || corelines[i][3] > (s.height/2 + s.height/4)))
+        if( ( coreline[2] < (s.width/2 - s.width/4) || coreline[2] > (s.width/2 + s.width/4) ) ||
+            (coreline[3] < (s.height/2 - s.height/4) || coreline[3] > (s.height/2 + s.height/4)))
         {
-            //circle(BW5,Point2d(corelines[i][2], corelines[i][3]), 6, Scalar(0,0,255),2);
-            output.emplace_back(corelines[i][2], corelines[i][3]);
+            //circle(circle,Point2d(corelines[i][2], corelines[i][3]), 6, Scalar(0,0,255),2);
+            output.emplace_back(coreline[2], coreline[3]);
         }
         else
         {
-            //circle(BW5,Point2d(corelines[i][2], corelines[i][3]), 6, Scalar(255,255,0),2);
-            centerpoints.emplace_back(corelines[i][2], corelines[i][3]);
+            //circle(circle,Point2d(corelines[i][2], corelines[i][3]), 6, Scalar(255,255,0),2);
+            centerpoints.emplace_back(coreline[2], coreline[3]);
         }
-        line( BW5, Point(corelines[i][0], corelines[i][1]),
-            Point(corelines[i][2], corelines[i][3]), Scalar(0,255,0), 2, 8 );
+        line( circleEnd, Point(coreline[0], coreline[1]),
+            Point(coreline[2], coreline[3]), Scalar(0,255,0), 2, 8 );
     }
 
     //resolve middle point of the wind turbine
@@ -261,82 +231,58 @@ vector<Point2d> Detector::detect(Mat frame){
         y_acc += centerpoint.y;
     }
 
-    if(!centerpoints.empty())
-    {
+    if(!centerpoints.empty()){
         output.emplace_back(x_acc / centerpoints.size(), y_acc / centerpoints.size());
-
     }
-    
     //debug print
-    for(size_t i = 0; i < output.size(); i++ )
-    {
-        circle(BW5,Point2d(output[i].x, output[i].y), 6, Scalar(255,0,0),2);
+    for(size_t i = 0; i < output.size(); i++ ){
+        circle(circleEnd,Point2d(output[i].x, output[i].y), 6, Scalar(255,0,0),2);
     }
-
     return output;
-
 }
 
-Rect Detector::lines2boundingbox(Mat frame, vector<Vec4i> lines)
-{
+Rect Detector::lines2boundingbox(Mat frame, vector<Vec4i> lines){
     int Xmax = frame.size[0]/2;
     int Xmin = frame.size[0]/2;
     int Ymax = frame.size[1]/2;
     int Ymin = frame.size[1]/2;
 
     Rect boundingBox;
-
-
-    for( size_t i = 0; i < lines.size(); i++ )
-    {
-
+    for( size_t i = 0; i < lines.size(); i++ ){
         //Create bounding box
-        if (Xmax < lines[i][0] || Xmax < lines[i][2] )
-        {
-            if( lines[i][0] > lines[i][2])
-            {
+        if (Xmax < lines[i][0] || Xmax < lines[i][2] ){
+            if( lines[i][0] > lines[i][2]){
                 Xmax = lines[i][0];
             }
-            else
-            {
+            else{
                 Xmax = lines[i][2];
             }
         }
-        if (Xmin > lines[i][0] || Xmin > lines[i][2] )
-        {
-            if( lines[i][0] < lines[i][2])
-            {
+        if (Xmin > lines[i][0] || Xmin > lines[i][2] ){
+            if( lines[i][0] < lines[i][2]){
                 Xmin = lines[i][0];
             }
-            else
-            {
+            else {
                 Xmin = lines[i][2];
             }
         }
-        if (Ymax < lines[i][1] || Ymax < lines[i][3] )
-        {
-            if( lines[i][1] > lines[i][3])
-            {
+        if (Ymax < lines[i][1] || Ymax < lines[i][3] ){
+            if( lines[i][1] > lines[i][3]){
                 Ymax = lines[i][1];
             }
-            else
-            {
+            else{
                 Ymax = lines[i][3];
             }
         }
-        if (Ymin > lines[i][1] || Ymin > lines[i][3] )
-        {
-            if( lines[i][1] < lines[i][3])
-            {
+        if (Ymin > lines[i][1] || Ymin > lines[i][3] ){
+            if( lines[i][1] < lines[i][3]){
                 Ymin = lines[i][1];
             }
-            else
-            {
+            else{
                 Ymin = lines[i][3];
             }
         }
     }
-
     boundingBox.x       = Xmin;
     boundingBox.y       = Ymin;
     boundingBox.width   = Xmax - Xmin; 
@@ -345,10 +291,7 @@ Rect Detector::lines2boundingbox(Mat frame, vector<Vec4i> lines)
     return boundingBox;
 }
 
-vector<Vec4i> Detector::lines2points( vector<Vec4i> lines )
-{
-    
-    
+vector<Vec4i> Detector::lines2points( vector<Vec4i> lines ){
     //Catogorize lines based on angle :o 
     std::vector<int> labels;
     int numberOfLines = cv::partition(lines, labels, isEqual());
@@ -357,18 +300,15 @@ vector<Vec4i> Detector::lines2points( vector<Vec4i> lines )
     vector<Vec4i> lines2;
 
     //For all labels
-    for(int i = 0; i < numberOfLines; i++)
-    {
+    for(int i = 0; i < numberOfLines; i++){
         vector<int> x;
         vector<int> y;
         bool big_x,big_y;
 
         //Check each line
-        for(size_t j = 0; j < lines.size(); j++)
-        {
+        for(size_t j = 0; j < lines.size(); j++){
             //if line has the correct label
-            if(labels[j] == i )
-            {
+            if(labels[j] == i ){
                 //Save values in the containers
                 x.push_back( lines[i][0]);
                 x.push_back( lines[i][2]);
@@ -377,21 +317,17 @@ vector<Vec4i> Detector::lines2points( vector<Vec4i> lines )
             }
             
             //Save whether x1 is bigger than x2. Could also be done with the slope
-            if(lines[i][0] < lines[i][2])
-            {
+            if(lines[i][0] < lines[i][2]){
                 big_x = 1;
             }
-            else
-            {
+            else{
                 big_x = 0;
             }
 
-            if(lines[i][1] < lines[i][3])
-            {
+            if(lines[i][1] < lines[i][3]){
                 big_y = 1;
             }
-            else
-            {
+            else{
                 big_y = 0;
             }
 
@@ -407,28 +343,23 @@ vector<Vec4i> Detector::lines2points( vector<Vec4i> lines )
         int x1,x2,y1,y2;
 
         //Write maxima to the output containers according the arrangement determined before
-        if(big_x)
-        {
+        if(big_x){
             x1 = x_min_value;
             x2 = x_max_value;
         }
-        else
-        {
+        else{
             x2 = x_min_value;
             x1 = x_max_value;
         }
         
-        if(big_y)
-        {
+        if(big_y){
             y1 = y_min_value;
             y2 = y_max_value;
         }
-        else
-        {
+        else{
             y2 = y_min_value;
             y1 = y_max_value;
         }
-
         //Add the found line the 
         lines2.push_back( Vec4i(x1, y1, x2 , y2 ) );
     }
--- main.cpp
+++ main.cpp
@@ -5,8 +5,6 @@
 #include "opencv2/highgui.hpp"
 //Class headers
 #include "detection.hpp"
-
-
 //Namespace decl.
 using namespace cv;
 
@@ -15,50 +13,61 @@ int main(int argc, const char** argv)
     // Declare the output variables
     Detector d;
     Mat frame;
+    auto video = false;
 
     //-------Static Image-------//
-//    const char* default_file = "images/7.png";
-//    const char* filename = argc >=2 ? argv[1] : default_file;
-//    // Loads an image
-//    frame = imread( samples::findFile( filename ), IMREAD_COLOR );
-//    // Check if image is loaded fine
-//    if(frame.empty()){
-//        printf(" Error opening image\n");
-//        printf(" Program Arguments: [image_name -- default %s] \n", default_file);
-//        return -1;
-//    }
-//    //Run the detector (detect houghlines)
-//    vector<Point2d> turbinePoints;
-//    turbinePoints = d.detect(frame);
-//    d.locate(frame);
-
-    //-------Video Feed-------//
-    VideoCapture cap("videos/2.mp4"); // open the default camera
-    if(!cap.isOpened()) { // check if we succeeded
-        std::cout << "cannot open camera "<< std::endl;
-        return -1;
-    }
-    //--- GRAB AND WRITE LOOP
-    std::cout << "Start grabbing" << std::endl
-         << "Press any key to terminate" << std::endl;
-    for (;;)
-    {
-        cap >> frame; // get a new frame from camera
-        // wait for a new frame from camera and store it into 'frame'
-        cap.read(frame);
-        // check if we succeeded
-        if (frame.empty()) {
-            std::cerr << "ERROR! blank frame grabbed\n";
-            break;
+    if (video == false){
+        const char* default_file = "images/4.png";
+        const char* filename = argc >=2 ? argv[1] : default_file;
+        // Loads an image
+        frame = imread( samples::findFile( filename ), IMREAD_COLOR );
+        cv::imshow("input", frame);
+        // Check if image is loaded fine
+        if(frame.empty()){
+            printf(" Error opening image\n");
+            printf(" Program Arguments: [image_name -- default %s] \n", default_file);
+            return -1;
         }
-
         //Run the detector (detect houghlines)
         vector<Point2d> turbinePoints;
         turbinePoints = d.detect(frame);
         d.locate(frame);
-        if (waitKey(5) >= 0)
-            break;
+        cv::imshow("Output", frame);
     }
+
+
+    //-------Video Feed-------//
+    if (video == true){
+        VideoCapture cap("videos/1.mp4"); // open the default camera
+        if(!cap.isOpened()) { // check if we succeeded
+            std::cout << "cannot open camera "<< std::endl;
+            return -1;
+        }
+        //--- GRAB AND WRITE LOOP
+        std::cout << "Start grabbing" << std::endl
+                  << "Press any key to terminate" << std::endl;
+        for (;;)
+        {
+            cap >> frame; // get a new frame from camera
+            // wait for a new frame from camera and store it into 'frame'
+            cap.read(frame);
+            // check if we succeeded
+            if (frame.empty()) {
+                std::cerr << "ERROR! blank frame grabbed\n";
+                break;
+            }
+
+            //Run the detector (detect houghlines)
+            vector<Point2d> turbinePoints;
+            turbinePoints = d.detect(frame);
+            //d.locate(frame);
+            // Display image.
+            cv::imshow("Output", frame);
+            if (waitKey(5) >= 0)
+                break;
+        }
+    }
+
 
     // Wait and Exit
     waitKey();
